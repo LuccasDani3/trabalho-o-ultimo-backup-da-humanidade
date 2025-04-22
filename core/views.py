e05e5c00545e37211f6a970c81e5a2ce6709ebf9
Luccas <luccas.daniel@mupisystems.com.br>
2025-04-22 15:01:33 -0300

    :wrench: Correções de redirecionamento e de do botão de scrollar novamente para cima

--- core/views.py
+++ core/views.py
@@ -31,7 +31,7 @@ class AcervoFixedListView(View):
 class AcervoCreateView(CreateView):
     model = Acervo
     form_class = AcervoForm
-    success_url = 'list/'
+    success_url = '/list/'
     template_name = 'core/create_acervo.html'
 
 
@@ -42,8 +42,9 @@ class AcervoListView(ListView):
     paginate_by = 8
 
     def get_queryset(self):
-        queryset = Acervo.objects.all()
+        queryset = Acervo.objects.all().order_by('-created_at')
 
+        filters = {}
         search_query = self.request.GET.get('search', '')
         if search_query:
             queryset = queryset.filter(
@@ -54,13 +55,13 @@ class AcervoListView(ListView):
 
         category_filter = self.request.GET.get('category', '')
         if category_filter:
-            queryset = queryset.filter(category=category_filter)
+            filters['category'] = category_filter
 
         date_filter = self.request.GET.get('date', '')
         if date_filter:
-            queryset = queryset.filter(estimated_date=date_filter)
+            filters['estimated_date'] = date_filter
 
-        return queryset
+        return queryset.filter(**filters)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
