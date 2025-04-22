c45f8d39d196f23853c386ef82ef644c7f13bca9
Luccas <luccasloyolamercadante@gmail.com>
2025-04-22 07:38:03 -0300

    :bug: Resolver pequenos bugs

--- core/forms.py
+++ core/forms.py
@@ -16,11 +16,12 @@ class AcervoForm(forms.ModelForm):
             'estimated_date': 'Data estimada',
         }
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Insira o título aqui...', 'required': True}),
-            'category': forms.Select(attrs={'required': True}),
-            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'placeholder': 'Descreva o tópico aqui...'}),
-            'image': forms.ClearableFileInput(attrs={'required': True}),
-            'estimated_date': forms.DateInput(attrs={'type': 'date'}),
+            'title': forms.TextInput(attrs={'placeholder': 'Insira o título aqui...', 'required': True, 'class': 'bg-transparent'}),
+            'category': forms.Select(attrs={'required': True, 'class': 'bg-transparent'}),
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'placeholder': 'Descreva o tópico aqui...', 'class': 'bg-transparent'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'file-input file-input-image bg-transparent', 'required': True}),
+            'file': forms.ClearableFileInput(attrs={'class': 'file-input file-input-document bg-transparent'}),
+            'estimated_date': forms.DateInput(attrs={'type': 'date', 'class': 'bg-transparent'}),
         }
     
     def __init__(self, *args, **kwargs):
